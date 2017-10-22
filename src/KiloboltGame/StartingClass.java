839e2b661bb0aef712db26e5e500b340d06446d6
StevenMeiklejohn <stevenmeiklejohn700@gmail.com>
2017-10-22 22:37:54 +0100

    Initial commit, starter and robot class complete, keylistener complete.

---
+++ src/KiloboltGame/StartingClass.java
@@ -0,0 +1,156 @@
+package KiloboltGame;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
+
+//Ctrl+Shift+O: Auto import code (may import the wrong classes)
+//Ctrl+Shift+F: Auto format code into proper indents
+//Ctrl+Space: Open auto complete suggestions.
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+
+	private Robot robot;
+	private Image image;
+	private Image character;
+	private URL base;
+	private Graphics second;
+
+	@Override
+	public void init() {
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+		try {
+			base = getDocumentBase();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		// Image Setups
+		character = getImage(base, "drawable/character.png");
+	}
+
+	@Override
+	public void start() {
+		robot = new Robot();
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			robot.update();
+			repaint();
+			try {
+				Thread.sleep(17);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			robot.moveLeft();
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			robot.moveRight();
+			break;
+
+		case KeyEvent.VK_SPACE:
+			robot.jump();
+			break;
+
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			System.out.println("Stop moving up");
+			break;
+
+		case KeyEvent.VK_DOWN:
+			System.out.println("Stop moving down");
+			break;
+
+		case KeyEvent.VK_LEFT:
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			robot.stop();
+			break;
+
+		case KeyEvent.VK_SPACE:
+			System.out.println("Stop jumping");
+			break;
+
+		}
+
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.drawImage(character, robot.getCenterX() - 61, robot.getCenterY() - 63, this);
+
+	}
+
+	@Override
+	public void update(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getWidth(), this.getHeight());
+			second = image.getGraphics();
+		}
+
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+
+		g.drawImage(image, 0, 0, this);
+
+	}
+
+}
